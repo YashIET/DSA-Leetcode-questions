4d74d52beb363cadb2f59cf21c4cf47f5c129e1e
YashIET <108289168+YashIET@users.noreply.github.com>
2023-06-26 17:58:42 +0530

    Time: 11 ms (85.24%), Space: 10.4 MB (65.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,24 +4,32 @@ public:
     //take current or not
     //what will be the states
     //current idx and previous idx
-    int rec(ll curr, ll pre, vector<ll>&nums,  vector<vector<ll>>&dp){
-        if(curr==nums.size())return 0;
+//     int rec(ll curr, ll pre, vector<ll>&nums,  vector<vector<ll>>&dp){
+//         if(curr==nums.size())return 0;
         
-        //not take current idx
-        if(dp[curr][pre+1]!=-1)return dp[curr][pre+1];
-        ll length = rec(curr+1,pre,nums, dp);
-        //take
-        // first check if curr can be taken or not
-        if(pre==-1 || nums[curr]>nums[pre])
-        length = max(length,1+rec(curr+1, curr,nums, dp));
+//         //not take current idx
+//         if(dp[curr][pre+1]!=-1)return dp[curr][pre+1];
+//         ll length = rec(curr+1,pre,nums, dp);
+//         //take
+//         // first check if curr can be taken or not
+//         if(pre==-1 || nums[curr]>nums[pre])
+//         length = max(length,1+rec(curr+1, curr,nums, dp));
         
-        return dp[curr][pre+1] =  length;
+//         return dp[curr][pre+1] =  length;
         
-    }
+//     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<ll>>dp(nums.size(),vector<ll>(nums.size()+1,-1));
-       ll ans= rec(0,-1,nums,dp);
-        return ans;
+        //2nd approach in nlogn time
+        vector<ll>temp;
+        temp.push_back(nums[0]);
+        for(ll i=1;i<nums.size();i++){
+            if(nums[i]>temp.back())temp.push_back(nums[i]);
+            else{
+                ll idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx]=nums[i];
+            }
+        }
+        return temp.size();
         
     }
 };
