0dd7d53d4f62f81ecb3f89e6e6bde3bfa6a52b01
YashIET <108289168+YashIET@users.noreply.github.com>
2023-07-17 21:45:50 +0530

    Time: 13 ms (39.67%), Space: 10.2 MB (14.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     typedef int ll;
-    ll ans=INT_MAX;
+    // ll ans=INT_MAX;
     ll n,m;
-    ll rec(ll row,ll col,vector<vector<int>>& grid,vector<vector<ll>>&dp){
-        if(row==n-1 && col==m-1){
-            return dp[row][col]= grid[row][col];
-        }
-        if(row==n || col==m){
-            return dp[row][col]= 100000000;
-        }
-        if(dp[row][col]!=-1)return dp[row][col];
+    ll rec(ll i,ll j,vector<vector<ll>>&grid, vector<vector<long long>>&ans){
+        if(i>=n || j>=m)return 1e9;
+        //check
+        if(ans[i][j]!=-1)return ans[i][j];
+        if(i==n-1 && j==m-1)return grid[i][j];
+        ll down=grid[i][j] + rec(i+1,j,grid,ans);
+        ll right=grid[i][j]+ rec(i,j+1,grid,ans);
         
-        //go bottom after check
-        ll bot=INT_MAX;
-        if(row<n-1)
-        bot=min(bot,grid[row][col]+ rec(row+1,col,grid,dp));
-        //go right
-        if(col<m-1)
-        bot=min(bot,grid[row][col] +rec(row,col+1,grid,dp));
-        return dp[row][col]=bot;
+        return ans[i][j]= (long long)min(down,right);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        
         n=grid.size();
         m=grid[0].size();
-        vector<vector<ll>>dp(n+1,vector<ll>(m+1,-1));
-        rec(0,0,grid,dp);
-        return dp[0][0];
+        
+        vector<vector<long long>>ans(n,vector<long long>(m,-1));
+       ll a= rec(0,0,grid,ans);
+        return a;
     }
 };
