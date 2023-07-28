8d5bbe4de0dc7a2bd9ee07825f0ea4a9f6361918
YashIET <108289168+YashIET@users.noreply.github.com>
2023-07-29 00:27:08 +0530

    Time: 24 ms (53.77%), Space: 14.4 MB (44.70%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    typedef int ll;
+    ll rec(ll i,ll buy,ll cap,vector<ll>&prices,vector<vector<vector<ll>>>&dp){
+        if(cap==0 || i==prices.size())return 0;
+        
+        if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
+        
+        ll profit=0;
+        if(buy){
+            profit=max(-prices[i]+rec(i+1,0,cap,prices,dp),rec(i+1,1,cap,prices,dp));
+        }
+        else{
+            profit=max(prices[i]+rec(i+1,1,cap-1,prices,dp),rec(i+1,0,cap,prices,dp));
+        }
+        return dp[i][buy][cap]=profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        ll n=prices.size();
+        vector<vector<vector<ll>>>dp(n,vector<vector<ll>>(2,vector<ll>(k+1,-1)));
+        return rec(0,1,k,prices,dp);
+    }
+};
