ed853ad7243e7b6000045a509d026aad267c2a02
YashIET <108289168+YashIET@users.noreply.github.com>
2023-07-28 23:26:25 +0530

    Time: 3 ms (95.77%), Space: 12.9 MB (99.23%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,19 +4,20 @@ public:
     
     int maxProfit(vector<int>& prices) {
         ll n=prices.size();
-        vector<ll>ahead(2),curr(2);
-        ahead[0]=0,ahead[1]=0;
+        // vector<ll>ahead(2),curr(2);
+        ll aheadbuy=0,aheadnotbuy=0,currbuy,currnotbuy;
+        aheadnotbuy=0,aheadbuy=0;
         for(ll i=n-1;i>=0;i--){
-            for(ll buy=0;buy<=1;buy++){
-                if(buy){
-                    curr[1]=max(-prices[i]+ahead[0],ahead[1]);
-                }
-                else{
-                    curr[0]=max(prices[i]+ahead[1],ahead[0]);
-                }
-            }
-            ahead=curr;
+            //loop not needed
+            
+               currbuy=max(-prices[i]+aheadnotbuy,aheadbuy);
+            
+            
+               currnotbuy=max(prices[i]+aheadbuy,aheadnotbuy);
+            
+            
+            aheadbuy=currbuy,aheadnotbuy=currnotbuy;
         }
-        return ahead[1];
+        return aheadbuy;
     }
 };
