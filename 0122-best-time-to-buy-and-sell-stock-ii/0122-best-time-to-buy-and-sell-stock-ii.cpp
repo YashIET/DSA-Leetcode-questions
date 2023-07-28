cd556e77d06aff1fb8ac1882691330b4cefe2ca9
YashIET <108289168+YashIET@users.noreply.github.com>
2023-07-28 23:22:49 +0530

    Time: 3 ms (95.77%), Space: 12.9 MB (79.08%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     typedef int ll;
-    ll rec(ll i,ll buy,vector<ll>&prices,vector<vector<ll>>&dp){
-        if(i>=prices.size())return 0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
-        ll profit=0;
-        if(buy){
-            profit=max(-prices[i]+rec(i+1,0,prices,dp),rec(i+1,1,prices,dp));
-        }
-        else profit=max(prices[i]+rec(i+1,1,prices,dp),rec(i+1,0,prices,dp));
-        
-        return dp[i][buy]=profit;
-    }
+    
     int maxProfit(vector<int>& prices) {
         ll n=prices.size();
-        vector<vector<ll>>dp(prices.size()+1,vector<ll>(2,-1));
-        dp[n][0]=0,dp[n][1]=0;
+        vector<ll>ahead(2),curr(2);
+        ahead[0]=0,ahead[1]=0;
         for(ll i=n-1;i>=0;i--){
             for(ll buy=0;buy<=1;buy++){
                 if(buy){
-                    dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                    curr[1]=max(-prices[i]+ahead[0],ahead[1]);
                 }
                 else{
-                    dp[i][buy]=max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                    curr[0]=max(prices[i]+ahead[1],ahead[0]);
                 }
             }
+            ahead=curr;
         }
-        return dp[0][1];
+        return ahead[1];
     }
 };
