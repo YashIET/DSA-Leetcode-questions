ea2a13c215027d0009f85503965f86ff969b1ac6
YashIET <108289168+YashIET@users.noreply.github.com>
2023-08-04 21:25:14 +0530

    Time: 257 ms (96.80%), Space: 10.4 MB (33.40%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -14,7 +14,17 @@ public:
         ll n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<ll>>dp(n+1,vector<ll>(n+1,-1));
-        return rec(1,n,nums,dp);
+        vector<vector<ll>>dp(n+2,vector<ll>(n+2,0));
+        for(ll i=n;i>=1;i--){
+            for(ll j=1;j<=n;j++){
+                if(i>j)continue;
+                ll maxi=INT_MIN;
+        for(ll ind=i;ind<=j;ind++){
+            maxi=max(maxi,nums[i-1]*nums[ind]*nums[j+1]+dp[i][ind-1]+dp[ind+1][j]);                
+        }
+         dp[i][j]= maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
