dc08f50196d068f2ec4e4147619b844e613dcd50
YashIET <108289168+YashIET@users.noreply.github.com>
2023-08-07 23:41:37 +0530

    Time: 1574 ms (56.04%), Space: 6.7 MB (57.53%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     typedef int ll;
-    bool isPalin(string &temp){
-        ll i=0,j=temp.size()-1;
+    bool isPalin(ll i,ll j,string &temp){
+        // ll i=0,j=temp.size()-1;
         while(i<j){
             if(temp[i++]!=temp[j--])return false;
             
@@ -13,11 +13,11 @@ public:
         if(i==s.size())return 0;
         if(dp[i]!=-1)return dp[i];
         ll mini=INT_MAX;
-        string temp="";
+        // string temp="";
         for(ll ind=i;ind<s.size();ind++){
-            temp+=s[ind];
+            // temp+=s[ind];
             
-            if(isPalin(temp)){
+            if(isPalin(i,ind,s)){
                 ll count=1+rec(ind+1,s,dp);
                 mini=min(mini,count);
             }
@@ -25,8 +25,23 @@ public:
         }
         return dp[i]= mini;
     }
+    //------------------------------------tabulation-------------------------------------
     int minCut(string s) {
-        vector<ll>dp(s.size(),-1);
-        return rec(0,s,dp)-1;
+        vector<ll>dp(s.size()+1,0);
+        ll n=s.size();
+        for(ll i=n-1;i>=0;i--){
+            ll mini=INT_MAX;
+       
+        for(ll ind=i;ind<n;ind++){
+                   
+            if(isPalin(i,ind,s)){
+                ll count=1+dp[ind+1];
+                mini=min(mini,count);
+            }
+            
+        }
+            dp[i]= mini;
+        }
+        return dp[0]-1;
     }
 };
