854445a7c4b898a944aa59f6ce9d17a19ece835f
YashIET <108289168+YashIET@users.noreply.github.com>
2023-08-12 01:13:38 +0530

    Time: 175 ms (97.79%), Space: 84 MB (82.78%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -2,28 +2,31 @@ class StockSpanner {
 public:
     typedef int ll;
     //store next greater to left:-->{nge,index};
-    vector<ll>v,temp;
+    // vector<ll>v,temp;
+    ll i=-1;
     stack<pair<ll,ll>>st;
     StockSpanner() {
         // v.push_back({0,0});
     }
     
     int next(int price) {
-        temp.push_back(price);
+        i++;
+        ll ans;
+        // temp.push_back(price);
        while(!st.empty() && st.top().first<=price){
            st.pop();
        }
         if(!st.empty()){
-            v.push_back(st.top().second);
-            
+            // v.push_back(st.top().second);
+            ans=i-st.top().second;
         }
         else{
-            v.push_back(-1);
-            
+            // v.push_back(-1);
+            ans=i+1;
         }
-        st.push({price,temp.size()-1});
+        st.push({price,i});
         //i-v[i] is the ans
-        return temp.size()-1-v[temp.size()-1];
+        return ans;
     }
 };
 
